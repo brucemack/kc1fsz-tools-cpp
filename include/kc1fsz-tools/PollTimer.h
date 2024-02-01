e4b1c5949f4b3866df98cacf5eff23c7e78714ca
brucemack <bruce@mackinnon.com>
2024-02-01 16:32:50 +0000

    UART buffer test working

---
+++ include/kc1fsz-tools/PollTimer.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2024, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+ */
+#ifndef _PollTimer_h
+#define _PollTimer_h
+
+#include <cstdint>
+
+namespace kc1fsz {
+
+class PollTimer {
+public:
+
+    virtual void setIntervalUs(uint32_t us) = 0;
+
+    /**
+     * @return true if the interval has expired.  Will only return 
+     *  true once per interval.
+    */
+    virtual bool poll() = 0;
+};
+
+}
+
+#endif
---
+++ include/kc1fsz-tools/rp2040/PicoPollTimer.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2024, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+ */
+#ifndef _PicoPollTimer_h
+#define _PicoPollTimer_h
+
+#include "kc1fsz-tools/PollTimer.h"
+
+namespace kc1fsz {
+
+class PicoPollTimer : public PollTimer {
+public:
+
+    PicoPollTimer();
+
+    void setIntervalUs(uint32_t i);
+    bool poll(); 
+
+private:
+
+    uint32_t _intervalUs;
+    absolute_time_t _last;
+};
+
+}
+
+#endif
