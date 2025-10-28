ad633ca5f7b929238975dd5bb9bf4fa9cb1cfc61
brucemack <bruce@mackinnon.com>
2025-10-28 15:40:17 -0400

    Working on new timer

---
+++ include/kc1fsz-tools/linux/LinuxPollTimer.h
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+ */
+#ifndef _LinuxPollTimer_h
+#define _LinuxPollTimer_h
+
+#include <cstdint>
+
+namespace kc1fsz {
+
+class LinuxPollTimer {
+public:
+
+    LinuxPollTimer(uint32_t us);
+
+    virtual void setIntervalUs(uint32_t us);
+
+    /**
+     * Starts the interval again. This is used for 
+     * synchronization - it doesn't need to be called
+     * explicitly on each interval.
+     */
+    virtual void reset();
+
+    /**
+     * @return true if the interval has expired.  Will only return 
+     *  true once per interval.
+    */
+    virtual bool poll();
+
+private:
+
+    uint32_t _intervalUs = 0;
+    uint64_t _startPointUs = 0;
+    uint64_t _nextPointUs = 0;
+};
+
+}
+
+#endif
---
+++ src/linux/LinuxPollTimer.cpp
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2024, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+*/
+#include "kc1fsz-tools/linux/LinuxPollTimer.h"
+
+#include <cstdint>
+#include <ctime>
+
+namespace kc1fsz {
+
+LinuxPollTimer::LinuxPollTimer(uint32_t us)
+:   _intervalUs(us) {   
+    reset();
+}
+
+void LinuxPollTimer::setIntervalUs(uint32_t us) {   
+    _intervalUs = us;
+    reset();
+}
+
+static uint64_t getTimeUs() {    
+    struct timespec ts;
+    if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
+        return ts.tv_sec * 1000000 + (ts.tv_nsec / 1000);
+    } else {
+        return 0;
+    }
+}
+
+void LinuxPollTimer::reset() {    
+    _startPointUs = getTimeUs();
+    _nextPointUs = _startPointUs + _intervalUs;    
+}
+
+bool LinuxPollTimer::poll() {  
+    uint64_t now = getTimeUs();
+    if (now >= _nextPointUs) {
+        _nextPointUs += _intervalUs;
+        return true;
+    } else {
+        return false;  
+    }
+}
+
+}
