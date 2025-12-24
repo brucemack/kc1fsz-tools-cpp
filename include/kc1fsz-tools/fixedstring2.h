d40cb8cbf220c0e6b7487f6a416e806327c270d0
Bruce MacKinnon <bruce@mackinnon.com>
2025-12-23 21:26:35 -0500

    New class

---
+++ include/kc1fsz-tools/fixedstring2.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <cstring>
+#include <cstdint>
+
+#include "Common.h"
+
+namespace kc1fsz {
+
+template <int N> class fixedstring2 {
+public:
+
+    fixedstring2() { clear(); }
+    fixedstring2(const fixedstring2& that) { strcpyLimited(_s, that._s, _size); }
+    fixedstring2(const char* s) { strcpyLimited(_s, s, _size); }
+    bool operator== (const char* other) const { return strcmp(_s, other) == 0; }
+    bool operator== (const fixedstring2& other) const { return strcmp(_s, other._s) == 0; }
+
+    const char* c_str() const { return _s; }
+    uint32_t length() const { return std::strlen(_s); }
+    uint32_t size() const { return length(); }
+    bool empty() const { return _s[0] == 0; }
+
+    void clear() {
+        _s[0] = 0;
+    }
+
+    void append(const char* s) {
+        for (unsigned i = 0; i < strlen(s); i++)
+            append(s[i]);
+    }
+
+    void append(const fixedstring2& s) {
+        append(s.c_str());
+    }
+
+    void append(char c) { 
+        uint32_t l = length();
+        if (l < _size - 1) {
+            _s[l] = c;
+            _s[l + 1] = 0;
+        }
+    }
+
+    void toUpper() {
+        for (uint32_t i = 0; i < _size && _s[i] != 0; i++)
+            _s[i] = toupper(_s[i]);
+    }
+
+private:
+
+    const uint32_t _size = N;
+    char _s[N];
+};
+
+}
