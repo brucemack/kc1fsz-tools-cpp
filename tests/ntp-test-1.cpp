fb64e54114a64282e8a33cfb8597c92195e3ff0b
Bruce MacKinnon <bruce@mackinnon.com>
2026-03-06 09:03:53 -0500

    new test cast

---
+++ tests/ntp-test-1.cpp
@@ -0,0 +1,97 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <iostream>
+#include <cstring>
+#include <cassert>
+
+#include "kc1fsz-tools/Common.h"
+#include "kc1fsz-tools/NetUtils.h"
+
+using namespace std;
+using namespace kc1fsz;
+
+static const char* NTP_IP_ADDR = "129.6.15.28";
+
+int query_1() {
+
+    const unsigned PACKET_SIZE = 128;
+    uint8_t packet[PACKET_SIZE];
+    int len = 0;
+    cout << len << endl;
+    if (len < 0)
+        return -1;
+
+    prettyHexDump(packet, len, cout);
+
+    // Make a UDP socket
+    int sockFd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockFd < 0) {
+        return -1;
+    }
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    // Bind to all interfaces
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(0);
+    if (::bind(sockFd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        cout << "Cant bing" << endl;
+        ::close(sockFd);
+        return -1;
+    }
+    // Make non-blocking
+    int flags = ::fcntl(sockFd, F_GETFL, 0);
+    if (flags == -1) {
+        cout << "Non blocking error" << endl;
+        ::close(sockFd);
+        return -1;
+    }
+    if (fcntl(sockFd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        cout << "Non blocking error" << endl;
+        ::close(sockFd);
+        return -1;
+    }
+
+    // Send the query to a DNS server
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(53);
+    inet_pton(AF_INET, DNS_IP_ADDR, &dest_addr.sin_addr); 
+    int rc = ::sendto(sockFd, packet, len, 0, 
+        (struct sockaddr*)&dest_addr, sizeof(dest_addr));
+    if (rc < 0) {
+        cout << "Send error " << endl;
+        return -1;
+    }
+
+    // RX loop
+
+    cout << "receive loop" << endl;
+
+    while (true) {
+        // Check for input
+        const unsigned readBufferSize = 512;
+        uint8_t readBuffer[readBufferSize];
+        struct sockaddr_in peerAddr;
+        socklen_t peerAddrLen = sizeof(peerAddr);
+        int rc = recvfrom(sockFd, readBuffer, readBufferSize, 0,
+            (struct sockaddr *)&peerAddr, &peerAddrLen);
+        if (rc > 0) {
+
+            prettyHexDump(readBuffer, rc, cout);
+
+        }
+    }
+}
+
+int main(int, const char**) {
+    query_1();
+}
