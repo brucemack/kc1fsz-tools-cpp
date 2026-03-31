a4fec3660d658c7c99aaf384513ee8f7da7eed6e
brucemack <bruce@mackinnon.com>
2026-03-31 10:28:49 -0400

    Basic Ethernet driver is working

--- include/kc1fsz-tools/ARPUtils.h
+++ include/kc1fsz-tools/ARPUtils.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024, Bruce MacKinnon KC1FSZ
+ * Copyright (C) 2026, Bruce MacKinnon KC1FSZ
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
